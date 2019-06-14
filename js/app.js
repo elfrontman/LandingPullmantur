1baad05a23bb2a63d82af66d619cea8b0144e241
Andrés Vargas <tcarlosvargas@gmail.com>
2019-06-13 23:55:45 -0500

    .

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation()
+
+$('.slider ul').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	prevArrow: '#prevArrow',
+	nextArrow: '#nextArrow',
+	dots: true
+});
